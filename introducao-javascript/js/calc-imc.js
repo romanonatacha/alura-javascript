e206e9df2ea1185b26dcaba0e3a81dc2c18b8256
natacha romano <natacharomanonr@gmail.com>
2019-07-21 21:17:11 -0300

    class 5

---
+++ introducao-javascript/js/calc-imc.js
@@ -0,0 +1,48 @@
+var title = document.querySelector(".title");
+
+title.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+    
+    if(peso <= 0 || peso > 1000){
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido  = false;
+        paciente.classList.add('paciente-invalido');
+    }
+    
+    if(altura <= 0 || altura >= 3){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+        paciente.classList.add('paciente-invalido');
+    }
+    
+    if (alturaEhValida && pesoEhValido) {
+    
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc.toFixed(2);    
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
+}
+
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura);
+    return imc.toFixed(2);
+}
